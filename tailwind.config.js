2a59be05116eae9f5baa0951db22fad4719e10eb
bahromiddin01 <bahromiddinabduqaxxorov1604@gmail.com>
2024-09-20 23:17:05 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,19 @@ module.exports = {
       fontFamily: {
         archivo: ['Archivo', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
       },
       colors: {
-        customGreen: '#05a105'
+        customGreen: '#05a105',
+        customCream: '#F4F2F0',
+        customDeepGreen: '#073630',
+        customDeepBrown: '#15130ECC',
+        customTransparentWhite: '#FFFFFFD9',
+        customCoal: '#1A1A1A',
+        customDeepGray: '#575757',
+        customForestGreen: '#027A48',
+        customBluishGray: '#475467',
+        customLightGray: '#D0D5DD',
       }
     },
   },
